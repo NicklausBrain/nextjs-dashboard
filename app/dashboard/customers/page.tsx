35f1a96c64aa483f8f86c44b56590c1638866c76
NICK BRAIN <nicklausbrain@gmail.com>
2024-05-19 17:09:05 +0300

    + test card

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
 export default function Page() {
-  return <p>Customers Page</p>;
+  const AnimalCoverUrl =
+    'https://tse4.mm.bing.net/th?id=OIP.eEVDp2Q9GipKs0JMf6y4SwHaFj&pid=Api';
+  return (
+    <main>
+      <a
+        href="#"
+        className="flex flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl md:flex-row"
+      >
+        <img
+          className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+          src={AnimalCoverUrl}
+          alt=""
+        ></img>
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Animals
+          </h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Animals are cute
+          </p>
+        </div>
+      </a>
+    </main>
+  );
 }
